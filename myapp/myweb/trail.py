efe66d8a5cd984ec8d0df1894c009bbc7a626cfa
bhanu sai prakash <98867581+bhanusp@users.noreply.github.com>
2022-04-05 12:17:11 +0530

    Delete myapp/myweb directory

--- myapp/myweb/trail.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask,render_template
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return"Hello this is the main page<h1>HIIIII</h1>"
-if __name__ == "__main__":
-    app.run()
